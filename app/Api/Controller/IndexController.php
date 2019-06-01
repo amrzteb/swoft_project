7f7d547b050b7b01f99f819f168901c0949be781
amtzteb <waps163com@163.com>
2019-06-01 15:39:37 +0800

    t

--- app/Api/Controller/IndexController.php
+++ app/Api/Controller/IndexController.php
@@ -3,6 +3,15 @@
 
 namespace App\Api\Controller;
 
+use Swoft;
+use Swoft\Bean\Exception\ContainerException;
+use Swoft\Context\Context;
+use Swoft\Http\Message\ContentType;
+use Swoft\Http\Message\Response;
+use Swoft\Http\Server\Annotation\Mapping\Controller;
+use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
+use Swoft\View\Renderer;
+
 /**
  * @Controller(prefix="/test1")
  */
